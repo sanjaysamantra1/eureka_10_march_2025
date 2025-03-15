4015f70ba4078709631620447d71a5ce416a64f0
Srinu Vunnam <vunnamsrinivas555@gmail.com>
2025-03-15 13:28:16 -0400

    Day2 Homework

---
+++ day2_homework.js
@@ -0,0 +1,11 @@
+//write a program, using prompt() ask user to enter 2 numbers, display addition result
+
+let x = parseInt(window.prompt("Enter first value"))
+ 
+let y = parseInt(window.prompt("Enter second value"))
+ 
+document.write("The result is ",x+y);
+
+
+
+
