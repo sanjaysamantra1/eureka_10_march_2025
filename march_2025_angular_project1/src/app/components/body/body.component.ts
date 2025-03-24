870cfb02dd9a298355031c0a52d1161af844da40
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-03-24 20:57:13 +0530

    data binding

--- march_2025_angular_project1/src/app/components/body/body.component.ts
+++ march_2025_angular_project1/src/app/components/body/body.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { ElectronicsComponent } from '../electronics/electronics.component';
+import { DataBindingComponent } from '../data-binding/data-binding.component';
 
 @Component({
   selector: 'app-body',
   imports: [
-    ElectronicsComponent
+    // ElectronicsComponent,
+    DataBindingComponent
   ],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css'
---
+++ march_2025_angular_project1/src/app/components/data-binding/data-binding.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-data-binding',
+  imports: [
+    FormsModule
+  ],
+  templateUrl: './data-binding.component.html',
+  styleUrl: './data-binding.component.css'
+})
+export class DataBindingComponent {
+  myName: string = 'Sachin Tendulkar';
+  img_url: string = 'https://seeklogo.com/images/A/angular-icon-logo-9946B9795D-seeklogo.com.png'
+  flag: boolean = false;
+  colspanNumber : number = 2;
+
+  addToCart() {
+    alert('Your Item is added to cart!!!')
+  }
+
+}
