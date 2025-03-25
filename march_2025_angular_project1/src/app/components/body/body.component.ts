556eedbf6f6279d866f475589bbb6edcb53c5edb
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-03-25 20:55:11 +0530

    data binding

--- march_2025_angular_project1/src/app/components/body/body.component.ts
+++ march_2025_angular_project1/src/app/components/body/body.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { ElectronicsComponent } from '../electronics/electronics.component';
 import { DataBindingComponent } from '../data-binding/data-binding.component';
 
@@ -9,7 +9,8 @@ import { DataBindingComponent } from '../data-binding/data-binding.component';
     DataBindingComponent
   ],
   templateUrl: './body.component.html',
-  styleUrl: './body.component.css'
+  styleUrl: './body.component.css',
+  // encapsulation: ViewEncapsulation.None
 })
 export class BodyComponent {
 
