b0a9b25f5128846cd0c46376a813b546891c54b1
Sanjay Samantra <sanjaysamantra1@gmail.com>
2025-03-26 20:54:14 +0530

    directives

--- march_2025_angular_project1/src/app/components/body/body.component.ts
+++ march_2025_angular_project1/src/app/components/body/body.component.ts
@@ -1,12 +1,14 @@
 import { Component, ViewEncapsulation } from '@angular/core';
 import { ElectronicsComponent } from '../electronics/electronics.component';
 import { DataBindingComponent } from '../data-binding/data-binding.component';
+import { DirectivesComponent } from '../directives/directives.component';
 
 @Component({
   selector: 'app-body',
   imports: [
     // ElectronicsComponent,
-    DataBindingComponent
+    // DataBindingComponent
+    DirectivesComponent
   ],
   templateUrl: './body.component.html',
   styleUrl: './body.component.css',
