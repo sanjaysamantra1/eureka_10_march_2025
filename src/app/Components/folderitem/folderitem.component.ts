84a3e8d446e022706c8d82eef514a871b7f0db61
shamanthularohan <shamanthularohan2212@gmail.com>
2025-04-07 23:41:27 -0400

    now

--- src/app/Components/folderitem/folderitem.component.ts
+++ src/app/Components/folderitem/folderitem.component.ts
@@ -1,21 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-
+import { faFolderOpen } from '@fortawesome/free-solid-svg-icons';
+import { faFile } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 @Component({
   selector: 'app-folderitem',
-  imports: [CommonModule],
+  imports: [CommonModule,
+    FontAwesomeModule
+  ],
   templateUrl: './folderitem.component.html',
   styleUrl: './folderitem.component.css',
   inputs:['node']
 })
 export class FolderitemComponent {
+  faFolderOpen = faFolderOpen;
+  faFile=faFile;
   node: any;
   isOpen = true;
   toggle() {
     this.isOpen = !this.isOpen;
   }
-  get iconClass(): string {
-    return 'bi ' + (this.isOpen ? 'bi-folder-fill' : 'bi-folder');
-  }
 
+  
 }
